997af80adcc856e2e18a5d76bf581f272cfa496d
ENISSAY39 <yassinebsofien@gmail.com>
2026-05-20 11:43:22 +0200

    Diamond DAG

---
+++ dags/q1_diamond.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from datetime import datetime
+from time import sleep
+
+from airflow.decorators import dag, task
+
+
+@dag(
+    dag_id="q1_diamond",
+    start_date=datetime(2026, 5, 1),
+    schedule=None,
+    catchup=False,
+    tags=["lab3"],
+)
+def q1_diamond():
+
+    @task.bash
+    def extract():
+        return 'echo "extracting..." && sleep 5'
+
+    @task
+    def transform_a():
+        sleep(3)
+        return "A done"
+
+    @task
+    def transform_b():
+        sleep(7)
+        return "B done"
+
+    @task
+    def load(a, b):
+        print(a)
+        print(b)
+        print("loaded")
+
+    e = extract()
+    a = transform_a()
+    b = transform_b()
+
+    l = load(a, b)
+
+    e >> [a, b] >> l
+
+
+q1_diamond()
